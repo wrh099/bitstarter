64624baa4cd7861a753de2f9da6c81a2a05166d3
billhallny@hotmail.com <billhall@twcny.rr.com>
2013-07-09 03:25:14 +0000

    correct web.js

--- web.js
+++ web.js
@@ -5,7 +5,7 @@ var msg = fs.readFileSync('index.html');
 
 app.get('/', function(request, response) {
   response.send(msg.toString());
-
+});
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
